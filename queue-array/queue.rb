e73a3ede5e3fb052f1bc33283687f1b77fa89ba8
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-10-28 12:28:47 +0100

    Create queue.rb

---
+++ queue-array/queue.rb
@@ -0,0 +1,30 @@
+class Queue
+    def initialize
+        @container=[]
+        @head=nil
+    end
+
+    def add_value value
+        if @head.nil?
+            @head=value
+        end
+        @container.push(value)
+    end
+
+    def remove
+        @container.shift
+        @head=@container[0]
+    end
+
+    def show_container
+        @container.join(" ")
+    end
+
+    def empty?
+        !@head.nil?
+    end
+
+    def peek
+        @head
+    end
+end
